f5985f8a0e8f80236921a66e9e2bc8f2b74f51d5
tpapp <tkpapp@gmail.com>
2011-05-12 15:27:08 +0200

    Cosmetic cleanup.

--- tests/package.lisp
+++ tests/package.lisp
@@ -1,6 +1,6 @@
-(defpackage #:cl-bayesian-tests
+(defpackage cl-bayesian-tests
   (:use common-lisp iterate bind anaphora alexandria cl-num-utils lla
         cl-random cl-bayesian lift)
   ;; also in alexandria
-  (:shadowing-import-from :cl-num-utils :mean :variance :xor)
+  (:shadowing-import-from cl-num-utils mean variance xor)
   (:export run-cl-bayesian-tests))
